bcc423fedfc4600e33675966060223622c8d0acf
Terris Kremer <terris.kremer@gmail.com>
2020-01-21 14:39:50 -0500

    update design

---
+++ src/components/home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useAuthUserContext } from '../session';
+import { ROUTES } from '../../constants';
+
+const HomePage = () => {
+  return(
+    <div data-testid="home-page">
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+export const Home = () => {
+  const { authUser } = useAuthUserContext();
+  return (
+    <div data-testid="home">
+      {authUser ? (<Redirect to={ROUTES.DASHBOARD} />) : (<HomePage />)}
+    </div>
+  )
+}
